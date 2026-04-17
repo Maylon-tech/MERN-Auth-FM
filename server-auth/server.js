2a042b24a70ee9f2f809020d08fa68c18696fbe2
Nemoto Mailon <minatokiroisen77@gmail.com>
2026-04-17 14:42:37 +0900

    starting tuto auth with Rest API

---
+++ server-auth/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("API is running...")
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log("Server is Running on PORT", PORT)
+})
